0b8606f2a52b5a61af515a0d353f4b2fcc7269b4
ipheghe <iphegheovie@gmail.com>
2019-01-23 18:05:40 +0100

    chore(heroku): Deploy to heroku
    This change addresses the need by:
    
    1.Setup `procfile`
    
    Delivers #163420779

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      welcomeText: "Welcome",
+      welcomeText: 'Welcome',
     };
   }
-  
+
   render() {
     return (
       <div>
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Root from './routes/routes'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './routes/routes';
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+ReactDOM.render(<Root />, document.getElementById('root'));
