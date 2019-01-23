a83405ddb8330883ee3055218525ae461d09024f
ipheghe <iphegheovie@gmail.com>
2019-01-23 14:15:19 +0100

    chore(react): Setup react and Parcel
    This change addresses the need by:
    
    1. Install all react dependencies
    2. install parcel-bundler dependency
    3. Setup folder structure
    4. create index.html file that renders react components
    
    Delivers #163420741

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      welcomeText: "Welcome",
+    };
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>{this.state.welcomeText}</h1>
+      </div>
+    );
+  }
+}
