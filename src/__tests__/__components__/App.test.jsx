7ec3d85f5fed11bde70a6f4e3b0f81f676dbe770
ipheghe <iphegheovie@gmail.com>
2019-01-23 17:18:47 +0100

    chore(TravisCi): Integrate Travis CI and add badge to README file
    This change addresses the need by:
    
    1. Create `.travis.yml` file and add config
    2. Add Travis badge to README file
    
    Delivers #163420764

---
+++ src/__tests__/__components__/App.test.jsx
@@ -0,0 +1,23 @@
+// react library
+import React from 'react';
+
+// third-party libraries
+import { shallow } from 'enzyme';
+import render from 'react-test-renderer';
+
+// components
+import App from '../../App';
+
+describe('App component', () => {
+  it('rshould match component snapshot', () => {
+    const tree = render.create(<App />);
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders without crashing', () => {
+    const shallowWrapper = shallow(<App />);
+    expect(shallowWrapper).toBeDefined();
+    expect(shallowWrapper.find('div').length).toBe(1);
+    expect(shallowWrapper.exists()).toBe(true);
+  });
+});
